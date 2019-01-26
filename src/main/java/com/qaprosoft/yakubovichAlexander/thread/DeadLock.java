9eb2a51a00d4a7a3cd09edb7084b274c1b5a3ccd
alexanderYakubovich <Sasha_112@tut.by>
2019-01-26 16:03:06 +0300

    Use formatter

--- src/main/java/com/qaprosoft/yakubovichAlexander/thread/DeadLock.java
+++ src/main/java/com/qaprosoft/yakubovichAlexander/thread/DeadLock.java
@@ -7,12 +7,12 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 public class DeadLock {
-	private final static Logger LOGGER = LogManager.getLogger(DeadLock.class);
+    private final static Logger LOGGER = LogManager.getLogger(DeadLock.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 	Lock lock1 = new ReentrantLock();
 	ThreadOne thread = new ThreadOne(lock1);
 	thread.start();
 
-}
+    }
 }
