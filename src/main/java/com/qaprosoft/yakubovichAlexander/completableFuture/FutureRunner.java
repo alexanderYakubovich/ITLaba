923ef68513c45b2ea198d3c1c205d0afa0b7b636
alexanderYakubovich <Sasha_112@tut.by>
2019-01-26 17:04:03 +0300

    Revert "Competable future is updated"
    This reverts commit 1c17a6349a9a060de76190324aa344737ec215b1.

--- src/main/java/com/qaprosoft/yakubovichAlexander/completableFuture/FutureRunner.java
+++ src/main/java/com/qaprosoft/yakubovichAlexander/completableFuture/FutureRunner.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.util.List;
 
 public class FutureRunner {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-	TextCompletableFuture textCompletableFuture = new TextCompletableFuture();
-	File file = new File("src/com/qaprosoft/yakubovichAlexander/main/resources/completableFuture/TextReader.txt");
-	List<String> stringLines = textCompletableFuture.read(file);
-	textCompletableFuture.write(stringLines);
-    }
+		TextCompletableFuture textCompletableFuture = new TextCompletableFuture();
+		File file = new File("src/com/qaprosoft/yakubovichAlexander/main/resources/completableFuture/TextReader.txt");
+		List<String> stringLines = textCompletableFuture.read(file);
+		textCompletableFuture.write(stringLines);
+	}
 }
